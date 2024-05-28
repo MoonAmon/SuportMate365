dfb958e8bce3f20b0d272186d208001552ac3315
MoonAmon <misterofclans.mo@gmail.com>
2024-05-28 19:56:17 -0300

    database fix function

--- src/database.py
+++ src/database.py
@@ -14,7 +14,7 @@ class DatabaseConnection:
         self.cursor = None
 
     def __enter__(self):
-        self.conn = psycopg2.connect(DATABASE_URL)
+        self.conn = Database.get_connection(DATABASE_URL)
         self.cursor = self.conn.cursor()
         logger.info('Database connection opened.')
         return self.cursor
